cade195661e84e856a29373a7eb054b6600786d4
Nosherwan Ijaz <nosherwanijaz@gmail.com>
2021-11-15 14:25:47 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Getting Started with AR JS React Viewer App
+
+### Install node and npm to run this project
+
+## `npm install`
+
+Before you run this project you need to install project dependencies.
+You can install the dependencies by excuting `npm install` command at the root directory of this project.
+
+## `npm start`
+
+Next, you need to run the app in the development mode. Run by executing `npm start` at the root of this project.
+
+Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
---
+++ src/index.js
@@ -0,0 +1,86 @@
+import React, { Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import '@grapecity/activereports/styles/ar-js-ui.css';
+import '@grapecity/activereports/styles/ar-js-viewer.css';
+import { Viewer } from '@grapecity/activereports-react';
+import '@grapecity/activereports/pdfexport';
+import '@grapecity/activereports/htmlexport';
+import '@grapecity/activereports/xlsxexport';
+import { Core } from '@grapecity/activereports';
+import { } from './arjs-license';
+
+const exportsSettings = {
+  pdf: {
+    title: 'ActiveReportsJS Sample',
+    author: 'GrapeCity',
+    subject: 'Web Reporting',
+    keywords: 'reporting, sample',
+    printing: 'none',
+    copying: false,
+    modifying: false,
+    annotating: false,
+    contentAccessibility: false,
+    documentAssembly: false,
+    pdfVersion: '1.7',
+    autoPrint: false,
+    filename: 'ActiveReportsJS-Sample',
+  },
+};
+
+async function downloadFonts() {
+  await Core.FontStore.registerFonts('assets/fontsConfig.json');
+  console.log('Fonts Registered');
+}
+
+function App() {
+  const [allExports, setAllExports] = React.useState([
+    { label: 'PDF', value: 'pdf', available: true },
+  ]);
+  downloadFonts();
+  function onCheckedChange(expValue) {
+    setAllExports((val) =>
+      val.map((exp) => {
+        if (exp.value === expValue)
+          return { ...exp, available: !exp.available };
+        return { ...exp };
+      })
+    );
+  }
+  return (
+    <Fragment>
+      <div className='container-fluid'>
+        <div className='form-group mb-3 mt-3'>
+          <div>
+            <label>Select available Exports: </label>
+          </div>
+          {allExports.map((exp) => (
+            <div className='form-check form-check-inline' key={exp.value}>
+              <input
+                className='form-check-input'
+                type='checkbox'
+                checked={exp.available}
+                onChange={() => onCheckedChange(exp.value)}
+              />
+              <label className='form-check-label'>{exp.label}</label>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div id='viewer-host'>
+        <Viewer
+          report={{
+            Uri: 'sample report/ColoradoCrash_Testing_v2.2.0.rdlx-json',
+          }}
+          exportsSettings={exportsSettings}
+          availableExports={allExports
+            .filter((exp) => exp.available)
+            .map((exp) => exp.value)}
+          sidebarVisible={true}
+        />
+      </div>
+    </Fragment>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
